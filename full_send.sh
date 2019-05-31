e4a3b6a7d100cdde48b7cd6b1a6102fb1255876d
Macfa <kombo6768@gmail.com>
2019-05-31 14:03:48 +0900

    send all backup data of server using postfix
    failed to separate content-type, and I'll send mail twice with different data

---
+++ full_send.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+# include files
+. /root/cron/mail/disk_space
+
+# define variable
+FS_RESULT=`df -h | grep -v "Used" | awk '{print $1,$5,$6}' | check_fs_space` # load function in disk_space file
+CURRENT_DATE=`date '+%Y%m%d'`; # current date
+
+# proccess on databases
+FULL_PATH="/backup/DB_BACKUP/${CURRENT_DATE}" # define full backup log path
+
+# moves work directory
+cd $FULL_PATH # change directory for use zip
+if [ ! -e "${CURRENT_DATE}.zip" ]
+then
+  zip -r ${CURRENT_DATE}.zip ./* # abstract all files and set name
+fi
+
+echo -e $FS_RESULT | mail -E -s "$(echo -e '211.117.60.181 FILESYSTEM INFO\nContent-type: text/html; charset=utf-8')" kombo67@naver.com
+echo -e "\n" | mail -s "$(echo -e '211.117.60.181 FULL BACKUP INFO\nContent-type: multipart/mixed;')" -a "${FULL_PATH}/${CURRENT_DATE}.zip" kombo67@naver.com,tmsk0711@naver.com,goonglee@naver.com
